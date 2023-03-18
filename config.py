890af24c4a6eff73d9470c4990696dffd95316d0
unknown <hirekaanm3@gmail.com>
2023-03-18 20:51:34 +0100

    .

--- config.py
+++ config.py
@@ -10,5 +10,6 @@ DEBUG = False
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://jackfros:kAZt3AnRHa7S0dPpZw7zEXdSoeQPZg1Qh@dpg-cgb0ppt269v4icoivk4g-a:5432/fyyur_app_db'
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+# 'postgresql://jackfros:kAZt3AnRHa7S0dPpZw7zEXdSoeQPZg1Qh@dpg-cgb0ppt269v4icoivk4g-a:5432/fyyur_app_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
